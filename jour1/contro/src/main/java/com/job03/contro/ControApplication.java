23210e2e65d57cadc756f0ec7e88c919f6c1e0aa
nilswen <nilswentingpro@gmail.com>
2023-09-18 17:16:31 +0200

    .

---
+++ jour1/contro/src/main/java/com/job03/contro/ControApplication.java
@@ -0,0 +1,15 @@
+package com.job03.contro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControApplication.class, args);
+	}
+
+	
+
+}
