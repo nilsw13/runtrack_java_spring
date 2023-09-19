d9b1b51c5f52723a777fd5e50db5fdbb79e13c69
nilswen <nilswentingpro@gmail.com>
2023-09-19 18:21:33 +0200

    TOUT LES JOBS DANS LA MEME APPLICATION

--- jour1/contro/src/main/java/com/job03/contro/ControApplication.java
+++ jour1/contro/src/main/java/com/job03/contro/ControApplication.java
@@ -8,25 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ControApplication {
 
-	private static String nom; 
+
 
 
 	public static void main(String[] args) {
-		if (args.length <= 0){
-            System.out.println("Veuillez fournir un argument pour definir le profil");
-            return;
-        }
-
-		nom = args[0];
 		
-
-
+		
 		SpringApplication.run(ControApplication.class, args);
+
+		
 	}
-
-
-	public static String getNom() {
-        return nom;
-    }
 	
 }
--- jour1/contro/src/main/java/com/job03/contro/Controler.java
+++ jour1/contro/src/main/java/com/job03/contro/Controler.java
@@ -1,20 +1,13 @@
 package com.job03.contro;
 
-
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class Controler {
-
-    
-    
 
 
     @Value("${greeting.message}")
@@ -22,15 +15,8 @@ public class Controler {
 
 
     @GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "world") String name){
-        
-        Date now = new Date();
-        SimpleDateFormat s = new SimpleDateFormat("dd/MM: HH:mm:ss");
-        String formated = s.format(now);
-        int a = 5;
-        int b = 5;
-        int result = a + b;
-        return String.format("hello %s, voici le resultat du calcul que tu ma demandé (%s + %s) :  %s ", ControApplication.getNom(),a,b, result);
+    private String getMsg(){
+        return greetingMessage;
     }
 
  
