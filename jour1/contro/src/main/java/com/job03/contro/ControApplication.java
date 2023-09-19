3426527d068945db8fc3dfaa1968c96deaa30a99
nilswen <nilswentingpro@gmail.com>
2023-09-19 03:22:22 +0200

    .

--- jour1/contro/src/main/java/com/job03/contro/ControApplication.java
+++ jour1/contro/src/main/java/com/job03/contro/ControApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ControApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ControApplication.class, args);
 	}
 
--- jour1/contro/src/main/java/com/job03/contro/Controler.java
+++ jour1/contro/src/main/java/com/job03/contro/Controler.java
@@ -1,14 +1,30 @@
 package com.job03.contro;
 
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controler {
 
     @GetMapping("/")
-    public String hello(){
-        return "Hello World";
+    public String hello(@RequestParam(value = "name", defaultValue = "world") String name){
+        name = "Nils";
+        Date now = new Date();
+        SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String formated = s.format(now);
+        int a = 5;
+        int b = 5;
+        int result = a + b;
+        return String.format("hello %s %s resultat : %s", name, formated, result);
     }
 
+ 
+
 }
+
