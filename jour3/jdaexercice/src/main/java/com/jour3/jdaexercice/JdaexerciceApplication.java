ff1422ad0e44d190215a10f54d6d22734dc8e7a2
nilswen <nilswentingpro@gmail.com>
2023-09-20 16:53:56 +0200

    .

---
+++ jour3/jdaexercice/src/main/java/com/jour3/jdaexercice/JdaexerciceApplication.java
@@ -0,0 +1,13 @@
+package com.jour3.jdaexercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdaexerciceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdaexerciceApplication.class, args);
+	}
+
+}
---
+++ jour3/jdaexercice/src/test/java/com/jour3/jdaexercice/JdaexerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jour3.jdaexercice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdaexerciceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
