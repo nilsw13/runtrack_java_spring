ce5c9c237ca0d0b631d8616f09b7ed22aca72dd2
nilswen <nilswentingpro@gmail.com>
2023-09-24 00:43:38 +0200

    .

--- jour4/app/src/main/java/com/finalday/app/Controllers/RegisterController.java
+++ jour4/app/src/main/java/com/finalday/app/Controllers/RegisterController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.finalday.app.models.UserModel;
 import com.finalday.app.models.Dto.RegistrationDTO;
 import com.finalday.app.services.AuthService;
+import com.finalday.app.services.UserService;
 
 @Controller
 public class RegisterController {
@@ -22,14 +24,14 @@ public class RegisterController {
     private AuthService authService;
     
     @GetMapping("/auth")
-    public String register(){
+    public String register(Model model, RegistrationDTO userDTO){
+        model.addAttribute("user", userDTO);
         return "auth";
     }
 
     @PostMapping("/register")
-    public String registerUser(@RequestBody RegistrationDTO body, Model model){
-        UserModel userModel = authService.registerUser(body.getUsername(), body.getPassword());
-        model.addAttribute("user", userModel);
+    public String registerUser(@RequestBody RegistrationDTO body){
+        authService.registerUser(body.getUsername(), body.getPassword());
         return "redirect:/auth"; 
     }
 }
